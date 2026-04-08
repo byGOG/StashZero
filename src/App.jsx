9b9279ab217fc533e466d1ec29d88b42170988f9
byGOG <Ond3rkor@gmail.com>
2026-04-08 13:50:58 +0300

    Updated Ninite and FMHY logos from official sources

--- src/App.jsx
+++ src/App.jsx
@@ -181,8 +181,8 @@ const SPECIAL_LOGOS = {
   officetoolplus: "https://officetool.plus/favicon.ico",
   cttwin: "https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/docs/static/favicon-32x32.png",
   sdi: "https://community.chocolatey.org/content/packageimages/sdio.1.17.8.829.png",
-  ninite: "https://ninite.com/static/images/ninite-logo.png",
-  fmhy: "https://fmhy.net/assets/img/fmhy-logo.png"
+  ninite: "https://ninite.com/static/img/brand-8a2e7c6ad769d6bc457c5c2b1a9743e8.png",
+  fmhy: "https://fmhy.net/pwa_icon.png"
 };
 
 const AppLogo = ({ id, className, ...props }) => {
